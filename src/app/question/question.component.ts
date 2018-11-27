76cac46d33a04b426178ba163c5fa456ecb5c8ac
Glenn Gartner <glenngartner@gmail.com>
2018-11-26 20:54:11 -0500

    implement question title length limit

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -8,7 +8,19 @@ import {StackQuestion} from '../generic/interfaces';
 })
 export class QuestionComponent implements OnInit {
 
-  @Input() question: StackQuestion;
+  private _question: StackQuestion;
+  private _titleCharLength = 35;
+
+  @Input()
+  set question(value: StackQuestion) {
+    if (value.title.length > this._titleCharLength) {
+      value.title = value.title.substr(0, this._titleCharLength) + '...';
+    }
+    this._question = value;
+  }
+  get question(): StackQuestion {
+    return this._question;
+  }
 
   constructor() {
   }
