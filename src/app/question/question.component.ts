4cdd60321c00c39982484ffad93d6d3a47468d9e
Glenn Gartner <glenngartner@gmail.com>
2018-12-03 23:09:13 -0500

    fix change detection strategy, so answer counts update in list view

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -6,7 +6,7 @@ import * as $ from 'jquery';
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class QuestionComponent implements OnInit, OnChanges {
 
